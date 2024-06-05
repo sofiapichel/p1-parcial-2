cec76000fa412e9e61972dab6a1ee70a3a6c1507
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2024-06-04 22:55:46 -0300

    chore: script basico

--- scripts/index.js
+++ scripts/index.js
@@ -1,56 +1,18 @@
 'use strict';
 
 /*
- *  APELLIDOS
+ * APELLIDO, NOMBRE | APELLIDO, NOMBRE
  */
 
+// Podria levantar los productos de un JSON o directamente estar escritos aca
+
 let productos = [
-    {
-        id: 1,
-        nombre: 'Producto',
-        descripcion: 'Descripción del producto',
-        precio: 0,
-        imagen: 'producto-de-ejemplo.jpg',
-        categoría: 'Nombre de la categoría',
-    },
-    {
-        id: 2,
-        nombre: 'Producto',
-        descripcion: 'Descripción del producto',
-        precio: 0,
-        imagen: 'producto-de-ejemplo.jpg',
-        categoría: 'Nombre de la categoría',
-    },
-    {
-        id: 3,
-        nombre: 'Producto',
-        descripcion: 'Descripción del producto',
-        precio: 0,
-        imagen: 'producto-de-ejemplo.jpg',
-        categoría: 'Nombre de la categoría',
-    },
-    {
-        id: 4,
-        nombre: 'Producto',
-        descripcion: 'Descripción del producto',
-        precio: 0,
-        imagen: 'producto-de-ejemplo.jpg',
-        categoría: 'Nombre de la categoría',
-    },
-    {
-        id: 5,
-        nombre: 'Producto',
-        descripcion: 'Descripción del producto',
-        precio: 0,
-        imagen: 'producto-de-ejemplo.jpg',
-        categoría: 'Nombre de la categoría',
-    },
-    {
-        id: 6,
-        nombre: 'Producto',
-        descripcion: 'Descripción del producto',
-        precio: 0,
-        imagen: 'producto-de-ejemplo.jpg',
-        categoría: 'Nombre de la categoría',
-    },
+    // {
+    //     id: 1,
+    //     nombre: 'Producto',
+    //     descripcion: 'Descripción del producto',
+    //     precio: 0,
+    //     imagen: 'producto-de-ejemplo.jpg',
+    //     categoría: 'Nombre de la categoría',
+    // }
 ];
